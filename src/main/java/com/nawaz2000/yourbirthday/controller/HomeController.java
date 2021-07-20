6fc744b7f36cade483dc1971da7ed1a71202efc8
Nawaz2000 <mail2nawaz2000@gmail.com>
2021-07-21 01:12:02 +0530

    default input date set

--- src/main/java/com/nawaz2000/yourbirthday/controller/HomeController.java
+++ src/main/java/com/nawaz2000/yourbirthday/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.nawaz2000.yourbirthday.controller;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 import javax.websocket.server.PathParam;
 
@@ -26,21 +32,22 @@ import com.nawaz2000.yourbirthday.service.HelpWithDate;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	BirthdayService service;
-	
+
 	@GetMapping("/")
-	public String showHome(Model model) {
+	public String showHome(Model model) { 
 		model.addAttribute("localDate", LocalDate.now());
 		return "home";
 	}
 	
+
 	@GetMapping("/birthdayInfo")
 	public String getBInfo(@PathParam(value = "date") String date, Model model) throws ParseException {
-		
+
 		String[] date2 = HelpWithDate.dateHelper(date);
-		
+
 		model.addAttribute("zodiac", service.findZodiac(Integer.parseInt(date2[2]), date2[1]));
 //		  System.out.println("Zodiac sign: " + service.findZodiac(Integer.parseInt(date2[2]), date2[1]));
 		model.addAttribute("birthDay", date2[0]);
@@ -48,22 +55,19 @@ public class HomeController {
 		model.addAttribute("age", service.age(date));
 //		  System.out.println("Age: " + service.age(date).get("Years") + " years "
 //				   + service.age(date).get("Months") + " months " + service.age(date).get("Days") + " days");
-		
+
 		model.addAttribute("chineseBirthAnimal", service.chineseBirthAnimal(date));
 //		  System.out.println("Chinese birth animal: " + service.chineseBirthAnimal(date));
-		  
+
 		model.addAttribute("birthStone", service.birthStone(date));
 //		  System.out.println("Birthstone: " + service.birthStone(date));
-		
-		
+
 		model.addAttribute("untilNextBirthDay", service.untilNextBirthday(date));
-		
+
 		model.addAttribute("aliveFor", service.aliveFor(date));
-		
-		
+
 		System.out.println(LocalDate.now());
 		return "redirect:/";
 	}
-	
-	
+
 }
